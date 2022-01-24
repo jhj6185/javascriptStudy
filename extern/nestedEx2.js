35d413b79b2d13cbd65b89f1d54cf4b770d00e40
jhj6185 <tkaqhcjstk+2@likelion.org>
2022-01-24 16:28:25 +0900

    javascript 배운내용 전부추가

---
+++ extern/nestedEx2.js
@@ -0,0 +1,3 @@
+document.write("nestedEx2.js는 nestedEx1.js에 포함");
+document.write("<div style='color: blue; font-size: 20px;'>외부 자바스크립트 파일 </div>");
+document.write("<script src='extern/nestedEx3.js'></script>");
